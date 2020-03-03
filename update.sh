fbea0382a388237ee56959c81a2a7fd38250dd77
Joseph Kligel <jkligel@gmail.com>
2020-03-03 15:32:03 -0600

    make changes to update script and it is now more fluid

--- update.sh
+++ update.sh
@@ -1,1 +1,8 @@
-sudo apt update && sudo apt-get update && sudo apt full-upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
+#!/bin/bash
+
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt autoremove -y
+sudo apt autoclean
+date >> ~/bin/update.log
+echo "\n Done updating"
