138397b6c428963559b205c6ba83ee967ebb0f85
Joseph Kligel <jkligel@gmail.com>
2020-05-21 01:23:11 -0500

    Using bash script to push all updates

---
+++ pushAll.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/$USER/bin; bash gitPush
+cd /home/$USER/cv; bash gitPush
+cd /home/$USER/python_programs; bash gitPush
+cd /home/$USER/c_programs; bash gitPush
+cd /home/$USER/node; bash gitPush
+cd /home/$USER/config; bash gitPush
+
+cd /home/$USER/Github/Programmapedia; bash gitPush
+cd /home/$USER/Github/budgetCrud; bash gitPush
+cd /home/$USER/Github/blog; bash gitPush
+cd /home/$USER/Github/Wiki-API; bash gitPush
+cd /home/$USER/Github/react; bash gitPush
+cd /home/$USER/Github/securitynode; bash gitPush
