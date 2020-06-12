ad26f3f853e41647de43a0ad8a7e0455c8ed9575
Joseph Kligel <jkligel@gmail.com>
2020-06-12 10:04:16 -0500

    Using bash script to push all updates

--- pushAll.sh
+++ pushAll.sh
@@ -7,9 +7,9 @@ cd /home/$USER/python_programs; bash gitPush
 cd /home/$USER/c_programs; bash gitPush
 cd /home/$USER/node; bash gitPush
 cd /home/$USER/config; bash gitPush
-cd /home/$USER/TetrisJS; bash gitPush
 cd /home/$USER/electron; bash gitPush
 cd /home/$USER/practice_python; bash gitPush
+cd /home/$USER/GamesJS; bash gitPush
 
 cd /home/$USER/Github/Programmapedia; bash gitPush
 cd /home/$USER/Github/budgetCrud; bash gitPush
