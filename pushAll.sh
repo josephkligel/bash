518c146332c630a049aa0118ed60dba97fd50636
Joseph Kligel <jkligel@gmail.com>
2020-06-28 06:47:46 -0500

    Using bash script to push all updates

---
+++ pushAll.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+git config --global credential.helper "cache --timeout 7200"
+
+cd /home/jkligel/bin; bash gitPush
+cd /home/jkligel/Github/python_programs/GamesJS; bash gitPush
+cd /home/jkligel/Github/python_programs/finance_py; bash gitPush
+cd /home/jkligel/Github/python_programs/Wiki-API; bash gitPush
+cd /home/jkligel/Github/python_programs/Bitcoin-tracker; bash gitPush
+cd /home/jkligel/Github/python_programs/electron; bash gitPush
+cd /home/jkligel/Github/python_programs/node; bash gitPush
+cd /home/jkligel/Github/python_programs/python_programs; bash gitPush
+cd /home/jkligel/Github/python_programs/Programmapedia; bash gitPush
+cd /home/jkligel/Github/python_programs/fruitProject; bash gitPush
+cd /home/jkligel/Github/python_programs/Head_First_Projects; bash gitPush
+cd /home/jkligel/Github/python_programs/Newsletter-Signup; bash gitPush
+cd /home/jkligel/Github/python_programs/todolist; bash gitPush
+cd /home/jkligel/Github/python_programs/calculatorjs; bash gitPush
+cd /home/jkligel/Github/python_programs/AI_py; bash gitPush
+cd /home/jkligel/Github/python_programs/websites; bash gitPush
+cd /home/jkligel/Github/python_programs/my-express-server; bash gitPush
+cd /home/jkligel/Github/python_programs/securitynode; bash gitPush
+cd /home/jkligel/Github/python_programs/config; bash gitPush
+cd /home/jkligel/Github/python_programs/cpp_programs; bash gitPush
+cd /home/jkligel/Github/python_programs/web-development; bash gitPush
+cd /home/jkligel/Github/python_programs/practice_python; bash gitPush
+cd /home/jkligel/Github/python_programs/cv; bash gitPush
+cd /home/jkligel/Github/python_programs/c_programs; bash gitPush
+cd /home/jkligel/Github/python_programs/gitignore; bash gitPush
+cd /home/jkligel/Github/python_programs/pythoncrud; bash gitPush
+cd /home/jkligel/Github/python_programs/java-programs; bash gitPush
+cd /home/jkligel/Github/python_programs/vba; bash gitPush
+cd /home/jkligel/Github/python_programs/blog; bash gitPush
+cd /home/jkligel/Github/python_programs/budgetCrud; bash gitPush
+cd /home/jkligel/Github/python_programs/react; bash gitPush
