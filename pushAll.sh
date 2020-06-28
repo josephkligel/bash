3210947d542c8143da3c0cda5e416a33722c2ff5
Joseph Kligel <jkligel@gmail.com>
2020-06-28 05:22:19 -0500

    Using bash script to push all updates

--- pushAll.sh
+++ pushAll.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 git config --global credential.helper 'cache --timeout 7200'
 
-cd /home/$USER/bin; bash gitPush
-cd /home/$USER/cv; bash gitPush
-cd /home/$USER/python_programs; bash gitPush
-cd /home/$USER/c_programs; bash gitPush
-cd /home/$USER/node; bash gitPush
-cd /home/$USER/config; bash gitPush
-cd /home/$USER/electron; bash gitPush
-cd /home/$USER/practice_python; bash gitPush
-cd /home/$USER/GamesJS; bash gitPush
-cd /home/$USER/AI_py; bash gitPush
-cd /home/$USER/cpp_programs; bash gitPush
+cd /home/$USER/Github/bin; bash gitPush
+cd /home/$USER/Github/cv; bash gitPush
+cd /home/$USER/Github/python_programs; bash gitPush
+cd /home/$USER/Github/c_programs; bash gitPush
+cd /home/$USER/Github/node; bash gitPush
+cd /home/$USER/Github/config; bash gitPush
+cd /home/$USER/Github/electron; bash gitPush
+cd /home/$USER/Github/practice_python; bash gitPush
+cd /home/$USER/Github/GamesJS; bash gitPush
+cd /home/$USER/Github/AI_py; bash gitPush
+cd /home/$USER/Github/cpp_programs; bash gitPush
 
 cd /home/$USER/Github/Programmapedia; bash gitPush
 cd /home/$USER/Github/budgetCrud; bash gitPush
