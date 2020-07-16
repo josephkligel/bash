2b9fb36f7dacf771438a0f12a71e37804000116c
Joseph Kligel <jkligel@gmail.com>
2020-07-15 23:48:54 -0500

    gitpy to your rescue

--- off.sh
+++ off.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
 
-gitpy --push
-updatefedora
-sudo poweroff
+OS_INFO=$(cat /etc/*-release)
+
+case $OS_INFO in
+
+	*"Fedora"*)
+	python /home/jkligel/Github/python_programs/gitpy/gitpy.py --push
+	updatefedora
+	sudo poweroff
+	;;
+
+	*"Centos"*)
+	python /home/jkligel/Github/python_programs/gitpy/gitpy.py --push
+	updaterehl
+	sudo poweroff
+	;;	
+
+	*"Manjaro"*)
+	python /home/jkligel/Github/python_programs/gitpy/gitpy.py --push
+	sudo pacman -Syyu
+	sudo poweroff
+	;;
+
+	*"Debian"*)
+	python /home/jkligel/Github/python_programs/gitpy/gitpy.py --push
+	updatedeb
+	sudo poweroff
+	;;	
+
+	*)
+	echo "Nothing was done"
+	;;
+esac
