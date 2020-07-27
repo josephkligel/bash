0195c0f3cd978df72effda1579bbf0926f3fc058
Joseph Kligel <jkligel@gmail.com>
2020-07-26 22:56:19 -0500

    gitpy to your rescue

--- setupLinux.sh
+++ setupLinux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 clone_git_repos(){
-	python3 $HOME/Github/python_programs/gitpy/gitpy --clone
+	python3 $HOME/Github/python_programs/gitpy/gitpy.py --clone
 }
 
 OS_INFO=$(cat /etc/*-release)
@@ -9,32 +9,32 @@ OS_INFO=$(cat /etc/*-release)
 case $OS_INFO in
 
 	*"Fedora"*)
-	clone_git_repos
 	bash installfed
+	clone_git_repos
 	bash updateLinux.sh	
 	;;
 
 	*"CentOS"*)
-	clone_git_repos
 	bash installrehl
+	clone_git_repos
 	bash updateLinux.sh
 	;;	
 
 	*"Manjaro"*)
-	clone_git_repos
 	bash installarch
+	clone_git_repos
 	bash updateLinux.sh	
 	;;
 
-	*"Debian"*)
-	clone_git_repos
+	*"Ubuntu"*)
 	bash installdeb
+	clone_git_repos
 	bash updateLinux.sh
 	;;
 
 	*"Arch"*)
-	clone_git_repos
 	bash installarch
+	clone_git_repos
 	bash updateLinux.sh	
 	;;
 
